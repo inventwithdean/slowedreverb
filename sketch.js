3dd99f13f46d3fb464f40d808f67cec76babef91
Abhay Singh <106145602+inventwithdean@users.noreply.github.com>
2024-06-30 17:33:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,8 +8,11 @@ function setup() {
   createCanvas(windowWidth, windowHeight / 2);
 
   soundFile = createFileInput(handleFile);
+  soundFile.size(windowWidth, 50);
   sliderRate = createSlider(0.1, 1, 0.8, 0.05);
+  sliderRate.size(windowWidth / 3, windowHeight / 50);
   sliderDrywet = createSlider(0, 0.75, 0.4, 0.05);
+  sliderDrywet.size(windowWidth / 3, windowHeight / 50);
   reverb = new p5.Reverb();
   amp = new p5.Amplitude();
 }
@@ -38,6 +41,6 @@ function draw() {
     reverb.drywet(sliderDrywet.value());
   }
   let val = amp.getLevel();
-  val = map(val, 0, 1, 50, 200);
+  val = map(val, 0, 1, windowWidth/5, windowWidth/3);
   ellipse(windowWidth / 2, windowHeight / 4, val, val);
 }
